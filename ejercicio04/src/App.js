c31d1b0e902662d79539515151890ed5d6ca3af5
CursoEGG001 <soygecko@gmail.com>
2023-09-01 20:59:47 -0300

    Crear un proyecto compuesto por 4 componentes bajo la siguiente jerarquía. • Index.js o App § Navbar § Main1 o Main2 § Footer Al hacer click sobre las dos posibles opciones en el NavBar, se deberá cambiar entre Main1 y Main2 dependiendo de la navegación. Para lograrlo se deberá instalar y usar React Router Dom. 40 A continuación, se propone como será la jerarquía de los componentes En la siguiente imagen se vera como establecer la navegación. Se sugiere ver los videos explicativos que encontrarán en el canal de Youtube de Egg.
    (correciones nuevas)

--- ejercicio04/src/App.js
+++ ejercicio04/src/App.js
@@ -9,7 +9,7 @@ import Home from './pages/Home';
 import PageNotFound from './pages/PageNotFound';
 
 function App() {
-         {/*
+    {/*
       * <Routes>
             <Route path={"/Direccion1"} element={ < ComponenteA / > } />
             <Route path={"/Direccion2"} element={ < ComponenteB / > } />
@@ -20,21 +20,21 @@ function App() {
          Aquí se inicia la elección de la presentación a elegir mediante  Routes  con distintos  Route  (sin la S del final)
          Usted puede cambiar las rutas por defecto a mostrar según se ingrese en barra de direcciones.
          Comentarios forzados por llaves y comentario de texto para poder remarcar el punto de elección de rutas.
-         */}   
-return (
-        <BrowserRouter>
-    <div className="App">
-        <Header />
-
-        <Routes>
-            <Route path={"/About"} element={ < About / > } />
-            <Route path={"/Home"} element={ < Home / > } />
-            <Route path={"/index.html"} element={ < Home / > } />
-            <Route path={"*"} element={ < PageNotFound / > } />
-        </Routes>
-        <Footer />
-    </div>
-</BrowserRouter>
-        );
-}
-export default App;
+         */
+    }
+    return (
+            <BrowserRouter>
+                <div className="App">
+                    <Header />
+                    <Routes>
+                        <Route path={"/About"} element={ < About / > } />
+                        <Route path={"/Home"} element={ < Home / > } />
+                        <Route path={"/index.html"} element={ < Home / > } />
+                        <Route path={"*"} element={ < PageNotFound / > } />
+                    </Routes>
+                    <Footer />
+                </div>
+            </BrowserRouter>
+                                    );
+                        }
+                        export default App;
