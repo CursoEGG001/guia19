be06c863654ee94b415a0f295adedc652a66a6f2
CursoEGG001 <soygecko@gmail.com>
2023-09-02 14:21:33 -0300

    Crear un proyecto compuesto de un solo componente y un servicio, quien deberá ser capaz de llamar desde el servicio, mediante la funcionalidad Fetch, a la API de Rick and Morty ( https://rickandmortyapi.com/api/character ) . Una vez llamado los datos desde el servicio, hacer uso de useEffect en el componente creado, deberá mostrar una lista compuesta de los nombres de los 20 primeros personajes.
    (correciones en presentación)

--- ejercicio06/src/components/public/Presentador.js
+++ ejercicio06/src/components/public/Presentador.js
@@ -5,32 +5,39 @@
 
 import React, { useState, useEffect } from 'react';
 import RickAndMortyServicio from '../../services/RickandMortyServicio';
-
 export const Presentador = () => {
     const [characters, setCharacters] = useState([]);
-
     useEffect(() => {
         // Llamamos a la API de Rick and Morty
         const charactersAPI = RickAndMortyServicio.getAllCharacters()
                 .then((data) => setCharacters(data.results.slice(0, 20)))
                 .catch((error) => console.log(error));
-
     }, []);
-
-
-
     return (
-            <div>
+            <div className='album'>
                 <h1>Personajes de Rick and Morty</h1>
                 <div className='container'>
                     <div className='row align-items-start justify-content-center'> 
-                        <div className='col-1'>
-                            <ul className='list-group'>
-                                {characters.map((character) => (
-                                        <li className='App-logo list-group-item focus-ring fs-2 order-{character.id}' key={character.id}>{character.name}</li>
-                                                        ))}
-                            </ul>
-                        </div>
+                        {characters.map((character) => (
+                                        <div className='col cols-auto'>
+                                            <div className='container'>
+                                                <div className='card focus-ring' style={{width: '18rem'}}>  
+                                                    <img src={character.image} className="App-logo card-img-top" alt={character.status}></img>
+                                                    <div clasName='card-body'>
+                                                        <h5 className="card-title">{character.name}</h5>
+                                                        <p className="card-text">{character.species}</p>
+                                                    </div>
+                                                    <ul className='list-group list-group-flush'>
+                                                        <li className='list-group-item fs-2' key={character.id}>{character.origin.name}</li>
+                                                    </ul>
+                                                    <div clasName='card-body'>
+                                                        <a href={character.location.url} className="card-link">{character.name} en Episodios</a>
+                                                        <a href={character.url} className="card-link">{character.name} en URL</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                                ))}
                     </div>
                 </div>
             </div>
